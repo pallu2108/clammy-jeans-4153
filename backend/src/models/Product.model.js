4d6fe9833482c4b5f6fcacedb8ae452f1e77cb34
ravipandeydu <rp120896@gmail.com>
2022-11-09 18:22:28 +0530

    Product Model Updated

--- backend/src/models/Product.model.js
+++ backend/src/models/Product.model.js
@@ -1,16 +1,27 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    Heading : {type : String, required : true},
-    Note : {type : String, required : true},
-    Tag: {type : String, required : true},
-    userId : {type : String, required : true}
-})
+  title: { type: String, required: true },
+  brand: { type: String, required: true },
+  rating: { type: Number, required: true },
+  mrp: { type: Number, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  images: { type: Array, required: true },
+  colors: { type: Array, required: true },
+  ddprice: { type: Map, required: true },
+  productDesc: { type: Map, required: true },
+  productFeatures: { type: Map, required: true },
+  category: {type: String, required: true},
+  subcategory: {type: String, required: true},
+  flash: {type: Boolean, default: false},
+  bestDeals: {type: Boolean, default: false},
+  trending: {type: Boolean, default: false},
+  newArrival: {type: Boolean, default: false},
+});
 
-const ProductModel = mongoose.model("note", productSchema)
-
+const ProductModel = mongoose.model("product", productSchema);
 
 module.exports = {
-    ProductModel
-}
+  ProductModel,
+};
