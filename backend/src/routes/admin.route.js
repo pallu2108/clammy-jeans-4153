340032db4f4edec475d43fb947083f568ad3f8e6
ravipandeydu <rp120896@gmail.com>
2022-11-11 16:22:27 +0530

    Admin Route created in Backend

---
+++ backend/src/routes/admin.route.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+const { userModel } = require("../models/User.model");
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const adminRoutes = Router();
+
+adminRoutes.post("/", async (req, res) => {
+  let { email, password } = req.body;
+  let user = await userModel.findOne({ email: email, role: "admin" });
+  if (user) {
+    let hash = user.password;
+    bcrypt.compare(password, hash, async function (err, result) {
+      if (result) {
+        var token = jwt.sign({ userId: user._id }, process.env.privateKey);
+        res.send({
+          message: "Login Successful",
+          token,
+        });
+      } else {
+        res.send("Something went wrong");
+      }
+    });
+  }
+  else{
+    res.send("Not authorized. Contact Admin")
+  }
+});
+
+module.exports = {
+  adminRoutes,
+};
